62299fe77048282c250630e65d0be4961ac2505e
Baron Tang <tangtang960225@163.com>
2024-09-14 14:56:01 +0800

    feat: 登陆路由守卫

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,53 @@
 // export default App;
 
 // new Router
-import { useRoutes, Link } from "react-router-dom";
+import { useRoutes, Link, useLocation, useNavigate } from "react-router-dom";
 import router from "./router";
-const App: React.FC = () => {
+import { useEffect } from "react";
+import { message } from "antd";
+
+const ToPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+  }, []);
+  return <></>;
+};
+
+const ToLogin = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    message.warning("请登录");
+    navigate("/login");
+  }, []);
+  return <></>;
+};
+
+const BeforeRouterEnter: React.FC = () => {
   const outlet = useRoutes(router);
+  const { pathname } = useLocation();
+
+  /**
+   * 如果访问的是登陆页面并且有token，跳转到首页
+   * 如果访问的不是登陆页，并且没有token，跳转到登陆页
+   */
+  const token = localStorage.getItem("ccc-token");
+  if (pathname === "/login" && token) {
+    return <ToPage />;
+  }
+  if (pathname !== "/login" && !token) {
+    return <ToLogin />;
+  }
+  return outlet;
+};
+
+const App: React.FC = () => {
   return (
     <div className="App">
       {/* <Link to="/home">home</Link> |
       <Link to="/about">about</Link> |
       <Link to="/user">user</Link> */}
-      {outlet}
+      <BeforeRouterEnter />
     </div>
   );
 };
